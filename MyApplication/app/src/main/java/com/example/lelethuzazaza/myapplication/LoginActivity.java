8f199ed36585288b90c1deba6eb09ceac61f1912
Lelethu <u13028023@tuks.co.za>
2015-09-17 12:25:48 +0200

    MyApp

---
+++ MyApplication/app/src/main/java/com/example/lelethuzazaza/myapplication/LoginActivity.java
@@ -0,0 +1,118 @@
+package com.example.lelethuzazaza.myapplication;
+
+import java.net.CookieHandler;
+import java.net.CookieManager;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.os.Bundle;
+import android.os.Vibrator;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginActivity extends Activity {
+	
+	public static final String TAG = LoginActivity.class.getSimpleName();
+	
+	private static final int DIALOG_LOADING = 0;
+		
+	Vibrator vb;
+	JSONArray userProfile;
+	SharedPreferences sp;
+	Editor ed;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.activity_login);
+		sp = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+		
+		ed = sp.edit();
+		
+		Button regiter_btn = (Button) findViewById(R.id.registerBtn);
+		regiter_btn.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent loginIntent = new Intent(LoginActivity.this,RegisterActivity.class);				
+				startActivity(loginIntent);
+			}
+		});
+		
+		
+		
+		Button login_btn = (Button) findViewById(R.id.loginBtn);
+		login_btn.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				//get a handle to the UI controls
+
+				EditText username = (EditText) findViewById(R.id.user_name);
+				EditText password = (EditText) findViewById(R.id.pass_word);
+
+				if (username.getText().toString().trim().length() == 0
+						|| password.getText().toString().trim().length() == 0) {
+
+					if (username.getText().toString().trim().length() == 0) {
+						username.setError("Username cannot be empty");
+					}
+
+					if (password.getText().toString().trim().length() == 0) {
+						password.setError("Password cannot be empty");
+					}
+
+				} else {
+
+					String u_name = username.getText().toString().trim();
+					String p_word = password.getText().toString().trim();
+
+					sendLoginRequest(u_name, p_word);
+
+				}
+			}
+		});
+	}
+
+	/**
+	 * Helper method to effect login request
+	 * @param username - username of the person trying to login (Cloud Tutor uses email addresses)
+	 * @param password - password to authenticate
+	 */
+	private void sendLoginRequest(String username, String password) {
+
+
+		
+	}		
+
+	
+	/**
+	 * Helper method to process the login response from Server
+	 * @param
+	 */
+	private void processLoginSuccessful() {
+
+
+	}
+
+	protected void onRuseme() {
+		super.onResume();
+	}
+	
+
+}
---
+++ MyApplication/app/src/main/java/com/example/lelethuzazaza/myapplication/RegisterActivity.java
@@ -0,0 +1,123 @@
+package com.example.lelethuzazaza.myapplication;
+
+import org.json.JSONObject;
+
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.os.Bundle;
+import android.os.Vibrator;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class RegisterActivity extends Activity {
+	private static String TAG = RegisterActivity.class.getSimpleName();
+	
+	Vibrator vb;
+	JSONObject user;
+	SharedPreferences sp;
+	Editor ed;
+	
+	Spinner spnServices;
+	
+	CheckBox chkTerms;
+	
+
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.activity_register);
+		sp = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+		ed = sp.edit();
+		Button register_btn = (Button) findViewById(R.id.submitRegister);		
+		
+		register_btn.setOnClickListener(onRegisterClickListener);
+		
+		spnServices = (Spinner)findViewById(R.id.spnServices);
+
+		
+		chkTerms = (CheckBox)findViewById(R.id.chkTerms);
+		
+
+		
+	}
+	
+	/**
+	 * 
+	 */
+	protected void onResume() {
+		super.onResume();
+	}
+	/**
+	 * 
+	 */
+	private OnClickListener onRegisterClickListener= new OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			EditText username = (EditText) findViewById(R.id.registerUsername);
+			EditText password = (EditText) findViewById(R.id.registerPassword);
+			EditText confirm_password = (EditText) findViewById(R.id.registerConfirmPassword);
+			EditText email_adress = (EditText) findViewById(R.id.registerEmail);
+			EditText surname = (EditText) findViewById(R.id.registerSurname);
+
+			Spinner role = (Spinner) findViewById(R.id.spnRole);
+
+			//user MUST agree to terms
+			if (!chkTerms.isChecked()) {
+				Toast.makeText(RegisterActivity.this, "You MUST agree to Terms and Conditions before you can register", Toast.LENGTH_LONG).show();
+				return;
+			}
+
+			if (username.getText().toString().trim().length() == 0
+					|| password.getText().toString().trim().length() == 0
+					|| confirm_password.getText().toString().trim().length() == 0
+					|| email_adress.getText().toString().trim().length() == 0
+					|| surname.getText().toString().trim().length() == 0) {
+
+				if (email_adress.getText().toString().trim().length() == 0) {
+					email_adress.setError("Email Cannot be empty");
+				}
+
+				if (username.getText().toString().trim().length() == 0) {
+					username.setError("Username Cannot be empty");
+				}
+
+				if (password.getText().toString().trim().length() == 0) {
+					password.setError("Password Cannot be empty");
+				}
+
+				if (confirm_password.getText().toString().trim().length() == 0) {
+					confirm_password
+							.setError("Confirm Password Cannot be empty");
+				}
+				if (surname.getText().toString().trim().length() == 0) {
+					surname.setError("Surname Cannot be empty");
+				}
+
+			} else {
+
+				if (!confirm_password.getText().toString().trim().equals(password.getText().toString().trim())) {
+					vb.vibrate(1000);
+					Toast.makeText(getApplicationContext(), "Passwords do not match...", Toast.LENGTH_LONG).show();
+					return;
+				}
+
+
+			}
+		}
+
+	};
+
+
+}
