23a82658b19c1ffca6f3835b06a42f3e71f4551b
Diana <diana.kawuma@gmail.com>
2015-09-30 19:44:08 +0200

    added new Application

---
+++ Personalassistant/app/src/main/java/imy320_android_assignment/personalassistant/MainActivity.java
@@ -0,0 +1,38 @@
+package imy320_android_assignment.personalassistant;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    Button bLogout;
+    EditText etName, etSurname, etUsername;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etName = (EditText) findViewById(R.id.etName);
+        etSurname = (EditText) findViewById(R.id.etSurname);
+        etUsername = (EditText) findViewById(R.id.etUsername);
+
+        bLogout = (Button) findViewById(R.id.bLogout);
+
+        bLogout.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.bLogout:
+                startActivity(new Intent(this, Login.class));
+                break;
+        }
+    }
+}
---
+++ Personalassistant/app/src/main/java/imy320_android_assignment/personalassistant/Register.java
@@ -0,0 +1,38 @@
+package imy320_android_assignment.personalassistant;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class Register extends AppCompatActivity implements View.OnClickListener{
+    Button bRegister;
+    EditText etName, etSurname, etUsername, etPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        etName = (EditText) findViewById(R.id.etName);
+        etSurname = (EditText) findViewById(R.id.etSurname);
+        etUsername = (EditText) findViewById(R.id.etUsername);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+        bRegister = (Button) findViewById(R.id.bRegister);
+
+        bRegister.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.bRegister:
+
+
+                break;
+        }
+    }
+}
