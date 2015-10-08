a9efadc97b5f7d8b2830f7bb9231432918818edc
Lelethu <u13028023@tuks.co.za>
2015-10-08 22:07:25 +0200

    Removal of database testing

--- Personalassistant/app/src/main/java/imy320_android_assignment/personalassistant/Login.java
+++ Personalassistant/app/src/main/java/imy320_android_assignment/personalassistant/Login.java
@@ -71,71 +71,14 @@ public class Login extends AppCompatActivity implements View.OnClickListener{
                     }
 
                 } 
-                else 
-                {
+                else {
                     //capture details
                     String u_name = etUsername.getText().toString().trim();
                     String p_word = etPassword.getText().toString().trim();
 
-                    //verify that user exists
-                    String result = null;
-                    InputStream is = null;
-                    StringBuilder sb=null;
 
-                    //http post
-                    try{
-                        //HttpClient httpclient = new DefaultHttpClient();
-                        //HttpPost httppost = new HttpPost("http://127.0.0.1/food.php");
-                        //httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-                        //HttpResponse response = httpclient.execute(httppost);
-                        //HttpEntity entity = response.getEntity();
-                        //is = entity.getContent();
-                    }catch(Exception e){
-                        Log.e("log_tag", "Error in http connection" + e.toString());
-                    }
-
-                    //convert response to string
-                    try{
-                        BufferedReader reader = new BufferedReader(new InputStreamReader(is,"iso-8859-1"),8);
-                        sb = new StringBuilder();
-                        sb.append(reader.readLine() + "\n");
-                        String line="0";
-
-                        while ((line = reader.readLine()) != null) {
-                            sb.append(line + "\n");
-                        }
-
-                        is.close();
-                        result=sb.toString();
-
-                    }catch(Exception e){
-                        Log.e("log_tag", "Error converting result "+e.toString());
-                    }
-
-                    //paring data
-                    int fd_id;
-                    String fd_name;
-                    try{
-                        JSONArray jArray = new JSONArray(result);
-                        JSONObject json_data=null;
-
-                        for(int i=0;i<jArray.length();i++){
-                            json_data = jArray.getJSONObject(i);
-                            fd_id=json_data.getInt("FOOD_ID");
-                            fd_name=json_data.getString("FOOD_NAME");
-                        }
-
-                    }catch(JSONException e1){
-                        Toast.makeText(getBaseContext(), "No Food Found", Toast.LENGTH_LONG).show();
-
-
-                   //go to user homepage
-
-                }}
-
-
-                startActivity(new Intent(this, MainActivity.class));
-
+                    startActivity(new Intent(this, MainActivity.class));
+                }
 
                 break;
 
